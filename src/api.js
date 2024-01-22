b9ba9736252e4e685c26b481c02becd15dfa7cdc
k-dor-ji <157406051+k-dor-ji@users.noreply.github.com>
2024-01-22 15:10:09 -0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoAPiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd491983be9msh052e3912a5d2b32p165629jsnfd7c6c49f70e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "290f0e950585e4e0f8d06aa34c684e5c";
